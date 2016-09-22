bc7c2e158882ac7e6e2fc13451f271159fc8f622
jstraw4663 <jstraw4663@gmail.com>
2016-09-22 09:45:56 -0400

    Delete acd_mount.sh

--- acd_mount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo 'Cantelope4' | ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -S /home/plex/.local-sorted/ /home/plex/local-sorted/
-echo 'Cantelope4' | ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -S /home/plex/.acd-sorted /home/plex/acd-sorted
-acd_cli mount /home/plex/.acd-sorted/
-unionfs-fuse -o cow /home/plex/local-sorted=RW:/home/plex/acd-sorted=RO /home/plex/sorted/
-mount -t tmpfs -o size=8G tmpfs /var/lib/plexmediaserver/Library/Transcoding
