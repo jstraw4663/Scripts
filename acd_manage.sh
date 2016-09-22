2ccc3a8cf87721f6313a14f8372651501cc1e3fc
jstraw4663 <plex@Ubuntu-1404-trusty-64-minimal>
2016-09-22 09:38:10 -0400

    Test

---
+++ acd_manage.sh
@@ -0,0 +1,28 @@
+echo "(===== Checks =====)"
+if [[ -d "/home/plex/acd-sorted/TvShows" ]];
+then
+echo "Properly Mounted";
+else
+echo "Crashed";
+echo "(===== Unmount =====)"
+echo "Unmount EncFS ACD"
+fusermount -z -u /home/plex/.acd-sorted
+
+echo "Unmount ACD"
+fusermount -z -u /home/plex/acd-sorted
+
+sleep 2
+
+echo "(===== Mount ACD =====)"
+echo "--> Clear Cache"
+# possible error here: when the db is corrupt cc fails
+acd_cli cc
+echo "--> Sync"
+acd_cli sync
+echo "--> Mount"
+acd_cli mount /home/plex/.acd-sorted/
+
+echo "(===== Mount EncFS =====)"
+echo 'Cantelope4' | ENCFS6_CONFIG='/path/to/encfs.xml' encfs -S /home/plex/.acd-sorted /home/plex/acd-sorted
+
+fi
